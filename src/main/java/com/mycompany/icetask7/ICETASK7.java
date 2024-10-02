a7cc80ba63bd3bfb4816a23ec7adb4ad2e658682
RC_Student_lab <RC_Student_lab@labG9AEB3.phdvvtwzx4oudioi2mg3lhtmwg.fx.internal.cloudapp.net>
2024-10-02 23:21:30 +0000

    Created scanner object

---
+++ src/main/java/com/mycompany/icetask7/ICETASK7.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.icetask7;
+import java.util.*;
+
+/**
+ *
+ * @author RC_Student_lab
+ */
+public class ICETASK7 {
+
+    public static void main(String[] args) {
+        //Create Scanner object
+       Scanner input = new Scanner(System.in);
+       
+       //Create an instance of a class
+       
+        
+    }
+}
